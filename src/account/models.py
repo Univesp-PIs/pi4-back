0cbd5b97c0e731746d5ffd1639211129a2cc8349
Brenno Brossi <brenno_workflow@outlook.com>
2025-04-24 19:57:05 -0300

    Rota Cost - OK

--- src/account/models.py
+++ src/account/models.py
@@ -22,3 +22,16 @@ class Credential(models.Model):
     def generate_token(self):
         payload = {'user_id': self.id}
         return jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    
+class EmailConfiguration(models.Model):
+    email = models.CharField(max_length=100)  # Endereço de e-mail usado para o envio
+    password = models.CharField(max_length=100)  # Senha do e-mail ou app password
+    smtp_server = models.CharField(max_length=100)  # Servidor SMTP (ex: smtp.gmail.com)
+    smtp_port = models.IntegerField()  # Porta SMTP (ex: 587 ou 465)
+    use_ssl = models.BooleanField(default=False)  # Indica se deve usar SSL/TLS
+    status = models.BooleanField(default=True)  # Ativo ou inativo
+    created_at = models.DateTimeField(auto_now_add=True)  # Data de criação
+    updated_at = models.DateTimeField(auto_now=True)  # Data de atualização
+
+    def __str__(self):
+        return f"{self.email} ({self.smtp_server}:{self.smtp_port})"
